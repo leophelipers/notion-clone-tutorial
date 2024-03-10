1389bf7374757868af0a2dec5a968cd2582196b1
Leo Phelipe <leo.phe.rod@gmail.com>
2024-03-10 00:00:30 -0300

    init

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://actual-glider-21.clerk.accounts.dev",
+      domain: "https://tight-mole-78.clerk.accounts.dev",
       applicationID: "convex",
     }
   ]
